f05dc71cb43358df295aa3c765b5553c3313c531
Evelio García Martin <eveliogarma@gmail.com>
2022-12-01 12:55:19 +0100

    create graphs

---
+++ graphs/stock_graphs.py
@@ -0,0 +1,196 @@
+import pandas
+class stock_graphs():
+    __data = {}
+    __layout = {}
+    __config = {
+        'stock':
+        {
+            'b_view': False,
+            'type': False,
+            'stock_name':"",
+            'currency_name':"",
+            'simple_col': "Close",
+            'simple_hover':"y",
+            'color': "#42C4F7",
+            'yaxis': "y1"
+        },
+        'rsi':
+        {
+            'b_view': False,
+            'pos':'below',
+            'type': False,
+            'domain':"",
+            'name': "Relative Strength Index",
+            'window':14,
+            'yaxis': "y2",
+            'simple_col': "rsi",
+            'simple_hover':"y",
+            'color':"#a85c32"
+        },
+        'ma':
+        {
+            
+            'b_view': False,
+            'pos':'below',
+            'type': False,
+            'name': "Moving Average",
+            'window': 14
+
+        },
+        "general":
+        {
+            'n_graphs':0,
+            'indicators':["rsi","ma"],
+            'domain':
+            { #config of the domain in the layout for diferent position
+                'two':
+                {
+                    'below':
+                    {
+                        'yaxis_domain':[0.3, 1],
+                        'yaxis2':{"domain": [0, 0.20]}
+                    },
+                     'over':
+                    {
+                        'yaxis_domain':[0, 0.20],
+                        'yaxis2':{"domain": [0.3, 1]}
+                    }
+                },
+                'three':
+                {
+                    'below':
+                    {
+                        'yaxis_domain':[0.4, 1],
+                        'yaxis2':{"domain": [0, 0.10]},
+                        'yaxis3':{"domain": [0.15, 0.30]}
+                    },
+                     'over':
+                    {
+                        'yaxis_domain':[0.2, 0.8],
+                        'yaxis2':{"domain": [0, 0.10]},
+                        'yaxis3':{"domain": [0.85, 1]}
+                    }
+                }
+            }
+        }
+    }
+    def __create_simple_layout(self):
+        self.__layout = dict(
+            autosize=True,
+            xaxis_rangeslider_visible=True,
+            showlegend=False,
+        )
+        return True
+    def __create_multiple_layout(self,ind:str):
+        self.__layout = dict(
+            autosize=True,
+            xaxis_rangeslider_visible=True,
+            showlegend=False,
+        )
+        self.__layout.update(self.__config[ind]["domain"])
+        return True
+    
+    def __add_simple(self,df: pandas.DataFrame,ind: str):
+        self.__data[ind] = dict( 
+            type="scatter",
+            y=df[self.__config[ind]['simple_col']].values,
+            x=df.index.values,
+            yaxis=self.__config[ind]["yaxis"],
+            line={"color": self.__config[ind]["color"]},
+            hoverinfo=self.__config[ind]['simple_hover'],
+            mode="lines",
+        )
+    
+    def __get_pos(self,ind: str, pos: str):
+        mask = [self.__config[b]["b_view"] for b in self.__config["general"]["indicators"]]
+        pos_n = [self.__config[b]["pos"] for b in self.__config["general"]["indicators"]]
+        n_graphs = "two"
+        conf = "below"
+        if any(mask) & (~self.__config[ind]["b_view"]):
+            n_graphs = "tree"
+            if pos_n[mask] == "over":
+                conf = "over"
+                pos = "below"
+        self.__config[ind]["pos"] = pos
+        self.__config[ind]["domain"] = self.__config["general"]["domain"][n_graphs][conf]
+        return True
+        
+    def add_stock(self,df: pandas.DataFrame,g_type:str):
+        """
+        Add stock configuration to a figure
+
+        Parameters:
+        df (pandas.DataFrame): dataframe that contains the column with numeric values to be calculated 
+        g_type (str): graph type can be "simple" for a line and "candels" if you want to change to candel type
+
+        Returns:
+        bool: true if added false if not. 
+        """
+        if g_type == "simple":
+            self.__add_simple(df,"stock")
+            self.__create_simple_layout()
+        elif g_type == "candels":
+            #draw candels
+            True
+        self.__config["stock"]["b_view"] = True
+        # self.update_graph()
+        return True
+    
+
+    
+    def add_rsi(self,df: pandas.DataFrame,g_type:str):
+        """
+        Add gri configuration to a figure
+
+        Parameters:
+        df (pandas.DataFrame): dataframe that contains the column with numeric values to be calculated 
+        g_type (str): graph type can be "over" for a line behind principal graph and "below" if you 
+        want a separated indicator graph.
+
+        Returns:
+        bool: true if added false if not. 
+        """
+        self.__config["stock"]["pos"] = g_type
+        if g_type == "on":
+            self.__config["rsi"]["yaxis"] = "y1"
+            self.__add_simple(df,"rsi")
+            self.__create_simple_layout()
+        elif g_type == "below":
+            self.__config["rsi"]["yaxis"] = "y2"
+            self.__add_simple(df,"rsi")
+            self.__get_pos("rsi",g_type)
+            self.__create_multiple_layout("rsi")
+            
+        self.__config["stock"]["b_view"] = True
+        return True
+    
+    def add_ma(self,df: pandas.DataFrame,g_type:str):
+        """
+        Add ma configuration to a figure
+
+        Parameters:
+        df (pandas.DataFrame): dataframe that contains the column with numeric values to be calculated 
+        g_type (str): graph type can be "over" for a line behind principal graph and "below" if you 
+        want a separated indicator graph.
+
+        Returns:
+        bool: true if added false if not. 
+        """
+        if g_type == "on":
+            #add line behind (addTrace)
+            True
+        elif g_type == "below":
+            #adline separate (addScatter)
+            True
+        return True
+
+    def update_graph(self):
+        
+        if "stock" not in self.__data.keys():
+            raise KeyError("At least need stock data to update the graph, add it")
+        
+        print(self.__data.values())
+        return go.Figure(
+            data = list(self.__data.values()),
+            layout= self.__layout,
+        )
---
+++ stats/gri.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import numpy as np
+
+def rsi_single(df,col,n): 
+    """
+    Calculate rsi for a column in a pandas dataframe
+  
+    Parameters:
+    df (pandas.DataFrame): dataframe that contains the column with numeric values to be calculated 
+    col (str): Column name in df that contins all numerics values. 
+  
+    Returns:
+    float: rsi value is between 0 to 100
+    """
+    delta = df[col].diff()
+    n = diff.shape[0]
+    delta_pos = diff[diff > 0].sum()/n
+    delta_neg = diff[diff < 0].sum()/n
+    if diff_neg:
+        rs = diff_pos / abs(diff_neg)
+        rsi = 100.0-(100.0)/(1.0 + rs)
+    else:
+        rsi = 100.0
+    return rsi
+
+def rsi(data: pd.Series, win = 14 , ma = "EWMA") -> pd.Series:
+    """
+    Calculate rsi for a column in a pandas dataframe. 
+    SMA calculates the average of price data, while EMA gives more weight to current data.
+  
+    Parameters:
+    over (pandas.Series): dataframe that contains the column with numeric values to be calculated 
+    w (int): Window of the rsi calculation
+  
+    Returns:
+    float: rsi value is between 0 to 100
+    """
+    if  ma == "EWMA":
+        fn_roll = lambda s: s.ewm(com=(win-1), adjust=True, min_periods = win).mean()
+    elif ma == "EMA":
+        fn_roll = lambda s: s.ewm(span = win,min_periods = win).mean()
+    elif ma == "SMA":
+        fn_roll = lambda s: s.rolling(win).mean()
+    elif ma == "RMA":
+        fn_roll = lambda s: s.ewm(alpha=1 / win).mean()
+    else:
+        raise ValueError(f"MA type: {ma} is not a valid method. Try EMA, SMA, RMA or EWMA")
+    
+    # Calculate diferences over every record from the previous one
+    delta = data.diff()
+
+    # Copy series. For ups and downs clip it value to 0 if negatives or positive respectively
+    up, down = delta.copy(), delta.copy()
+    
+    up[up < 0] = 0
+    down[down > 0] = 0
+    down *= -1
+    
+    win_up = fn_roll(up) 
+    win_down =  fn_roll(down)
+  
+    rs = win_up / win_down
+    rsi = 100.0 - (100.0 / (1.0 + rs))
+    
+    # if win_down == 0 --> RS --> inf and rsi = 100
+    # if win_up == 0 --> RS --> 0 and rsi = 0
+    rsi[:] = np.select([win_down == 0, win_up == 0, True], [100.0, 0.0, rsi])
+    rsi.name = 'rsi'
+
+    # Assert that rsi (from win value in data (taking into account the first nan from diff) is
+    # between 0 and 100. 
+    check_rsi = rsi[length:]
+    assert ((0 <= check_rsi) & (check_rsi <= 100)).all()
+    
+    return rsi
